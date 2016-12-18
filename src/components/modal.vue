e15e9390f628ca8ce874fd512ff6e1a9c0509024
@dokstep <jorgecbll@gmail.com>
2016-12-17 22:20:54 -0400

    Added modal with about info, and progress bar

---
+++ src/components/modal.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+            <div class="modal-content">
+                <slot></slot>
+            </div>
+        <slot name="close"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
--- src/components/timer.vue
+++ src/components/timer.vue
@@ -1,65 +1,117 @@
 <template>
-	<div class="container has-text-centered is-mobile">	
-		<div class="primary-buttons">
-			<button class="button is-primary" @click="session">Pomodoro</button>
-			<button class="button is-primary" @click="shortBreak">Short Break</button>
-			<button class="button is-primary" @click="longBreak">Long Break</button>
-			<p class="auto_start">
-				Status: {{state}}
-			</p> 
-			<span>
-				Auto start timer: 
-				<input type="checkbox" v-model="autoStart" class="checkbox">			
-			</span>
-		</div>
-		<div id="time"> 
-			<div v-show="!sessionStarted" class="icon is-large clickable" @click="up">
-					<i class="button is-primary fa fa-caret-up" ></i>
-				</div>
-				<h1 class="title">       
-						{{minutes}}
-				</h1>
-				<h2 class="subtitle">
-						{{seconds}}
-				</h2>
-				<div v-show="!sessionStarted" class="icon is-large clickable" @click="down">
-					<i class="button is-primary fa fa-caret-down" ></i>
-				</div>
-			</div>
-			<div class="buttons"> 
-				<a class="button is-primary is-large" :class="{'is-disabled': sessionStarted}" @click="start">Start</a>
-				<a class="button is-primary is-large" :class="{'is-disabled': !sessionStarted}" @click="stop">Stop</a>
-				<a class="button is-primary is-large" :class="{'is-disabled': sessionStarted}" @click="reset">Reset</a>
-		</div>
-	</div>  
+
+   <div class="hero is-fullheight">   
+        <div class="container has-text-centered is-mobile">	
+            <!--<nav class="nav has-text-centered">
+                <div class="container">
+                    <div class="nav-center">
+                        <a class="nav-item is-tab">
+                        <button class="button is-primary" @click="openModal">About</button> 
+                        </a>
+                        <a class="nav-item is-tab">
+                            <button class="button is-primary" @click="session">Pomodoro</button>
+                        </a>
+                        <a class="nav-item is-tab">
+                            <button class="button is-primary" @click="shortBreak">Short Break</button>
+                        </a>
+                        <a class="nav-item is-tab">
+                            <button class="button is-primary" @click="longBreak">Long Break</button>
+                        </a>
+                    </div>             
+                </div>
+
+            </nav>-->
+            <div class="primary-buttons">
+                <button class="button is-primary" @click="openModal">About</button>
+                <button class="button is-primary" @click="session">Pomodoro</button>
+                <button class="button is-primary" @click="shortBreak">Short Break</button>
+                <button class="button is-primary" @click="longBreak">Long Break</button>
+
+                <modal v-if="modalToggle">
+                    <div class="card has-text-left is-pulled-left">
+                        <div class="card-content">                                                    
+                            <div class="media-content">                                                          
+                                <p class="title is-2">Pomodoro Technique</p>
+                            </div>
+														<br>	
+                            <div class="content">
+                            The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. 
+                            The technique uses a timer to break down work into intervals, traditionally 25 minutes in length, 
+                            separated by short breaks. These intervals are named pomodoros, 
+                            the plural in English of the Italian word pomodoro (tomato), 
+                            after the tomato-shaped kitchen timer that Cirillo used as a university student.
+                            <br>
+                            <a class="twitter" href="https://en.wikipedia.org/wiki/Pomodoro_Technique" target="_blank">Read more</a>
+                            </div>
+														<footer>
+															<small class="twitter is-pulled-right">
+																made by <a href="https://twitter.com/dokstep">@dokstep</a>
+															</small>
+														</footer>
+
+                        </div>
+                    </div>
+                    <button slot="close" class="modal-close" @click="openModal"></button>
+                </modal>
+                <p class="auto_start">
+                    Status: {{state}}
+                </p> 
+                <span>
+                    Auto start timer: 
+                    <input type="checkbox" v-model="autoStart" class="checkbox">			
+                </span>
+            <div id="time"> 
+                <div v-show="!sessionStarted" class="icon is-large clickable" @click="up">
+                        <i class="button is-primary fa fa-caret-up" ></i>
+                </div>
+
+                    <h1 class="title">{{minutes}}</h1>
+                    <h2 class="subtitle" >{{seconds}}</h2>
+                    <progress class="progress is-primary is-small" :value="progress" :max="timeMax"></progress>
+                    <div v-show="!sessionStarted" class="icon is-large clickable" @click="down">
+                        <i class="button is-primary fa fa-caret-down" ></i>
+                    </div>
+                </div>
+                <div class="buttons"> 
+                    <a class="button is-primary is-large" :class="{'is-disabled': sessionStarted}" @click="start">Start</a>
+                    <a class="button is-primary is-large" :class="{'is-disabled': !sessionStarted}" @click="stop">Stop</a>
+                    <a class="button is-primary is-large" :class="{'is-disabled': sessionStarted}" @click="reset">Reset</a>
+                </div>
+        </div>
+    </div> 
+	
 </template>
 
 <script>
+import modal from '../components/modal.vue'
+import _ from 'lodash'
     var time;
 
     export default {
 
         mounted() {
-            this.askNotify()
+            
         },
         data() {
             return {
                 sessionTime: 1500,
+                timeMax: 1500,
                 state: 'Idle',
                 status: false,
                 autoStart: false,
                 sessionStarted: false,
-                breakStarted: false,
                 date: '',
-                finish: ''
+                finish: '',
+                modalToggle: false
             }
         },
         methods: {
             timer() {
+                this.title()     
                 if (this.sessionTime === 0) {
                     clearInterval(time)
                     var d = new Date()
-                    this.finish = `${d.getHours()}:${d.getMinutes()}`
+                    this.finish = d.getMinutes()
                     this.sessionStarted = false
                     this.status = true
                     this.state = 'Idle'
@@ -68,9 +120,13 @@
                     return this.sessionTime--;
                 }
             },
+            title(){
+                document.title = `Pomodoro ${this.minutes}:${this.seconds}` || 'Pomodoro'
+            },
             session() {
                 clearInterval(time)
                 this.sessionTime = 1500
+                this.timeMax = 1500
                 this.state = 'Pomodoro.'
                 if (this.autoStart) {
                     this.start()
@@ -84,10 +140,9 @@
                     this.state = 'Working!'
                     var d = new Date()
                     this.sessionStarted = true
-                    this.date = `${d.getHours()}:${d.getMinutes()}`
-                    time = setInterval(this.timer, 1000)
+                    this.date = d.getMinutes()
+                    time = setInterval(this.timer, 1000)            
                 }
-
             },
             stop() {
                 clearInterval(time)
@@ -102,6 +157,7 @@
             shortBreak() {
                 clearInterval(time)
                 this.sessionTime = 300
+                this.timeMax = 300
                 this.sessionStarted = false
                 this.state = 'Short break.'
                 if (this.autoStart) {
@@ -111,6 +167,7 @@
             longBreak() {
                 clearInterval(time)
                 this.sessionTime = 900
+                this.timeMax = 900
                 this.state = 'Long break. Get coffee.'
                 this.sessionStarted = false
                 if (this.autoStart) {
@@ -128,10 +185,10 @@
                 }
             },
             notify(text) {
+                console.log(this)
                 var options = {
                     icon: 'src/assets/tomato.png',
                 }
-
                 // Let's check whether notification permissions have already been granted
                 if (Notification.permission === "granted") {
                     // If it's okay let's create a notification
@@ -139,7 +196,6 @@
                     var audio = new Audio('src/assets/audio/bloop_x.wav')
                     audio.play()
                 }
-
                 // Otherwise, we need to ask the user for permission
                 else if (Notification.permission !== 'denied') {
                     Notification.requestPermission(function(permission) {
@@ -154,6 +210,9 @@
                 Notification.requestPermission().then(function(result) {
                     console.log(result);
                 })
+            },
+            openModal(){
+                this.modalToggle = !this.modalToggle
             }
         },
         computed: {
@@ -161,42 +220,80 @@
                 return Math.floor((((this.sessionTime % 31536000) % 86400) % 3600) / 60)
             },
             seconds() {
-                return (((this.sessionTime % 31536000) % 86400) % 3600) % 60
+                if(((((this.sessionTime % 31536000) % 86400) % 3600) % 60) >= 10){
+                    return ((((this.sessionTime % 31536000) % 86400) % 3600) % 60)
+                } else {
+                    return '0'+((((this.sessionTime % 31536000) % 86400) % 3600) % 60)
+                }
             },
+            progress(){
+                return (this.timeMax - this.sessionTime) 
+            },
+
+        },
+        components: {
+            modal
         }
     }
 </script>
 
 <style lang="sass" src="../../node_modules/bulma/bulma.sass"></style>
 <style lang="sass">
-    html {
-        background-color: #1A1B3A;
-        max-height: 100vh;
-        max-width: 100vw;
-    }
-    
-    p,
-    span {
-        color: #b33353;
-        font-size: 24px
-    }
-    
-    .title {
-        font-weight: bold;
-    }
-    
-    .title,
-    .subtitle {
-        font-size: 20vh;
-        color: #F62554
-    }
-    
-    .primary-buttons {
-        padding-bottom: 25px;
-        padding-top: 10px;
-    }
-    
-    div.clickable {
-        cursor: pointer;
-    }
+.twitter a {
+	color: #F62554;
+}
+.content {
+	color: #c46f7f;
+}
+.content a:not(.button):visited {
+    color: #F62554;
+}
+.card-content {
+	background-color: #1A1B3A;
+}
+.text {
+    color: #1A1B3A;
+}
+.hero {
+    background-color: #1A1B3A;
+}
+.bg {
+    background-color: #F62554;
+}
+html {
+    max-height: 100vh;
+    max-width: 100vw;
+}
+
+p,
+span {
+    color: #F62554;
+    font-size: 24px
+}
+
+.title {
+    font-weight: bold;
+}
+
+.title,
+.subtitle {
+    font-size: 20vh;
+    color: #F62554 
+}
+
+/*#F62554*/
+
+.primary-buttons {
+    padding-bottom: 25px;
+    padding-top: 10px;
+}
+
+div.clickable {
+    cursor: pointer;
+}
+
+.hero .nav {
+    background: none;
+    box-shadow: 0 0px 0 rgba(0, 0, 0, 0);
+}
 </style>
